2de608d05b80cf6d3be3d95ec3d662bb55e9f8ea
Joshua Terranova <terranovajoshua1@gmail.com>
2026-07-03 17:11:05 -0700

    Implement wordle and schedwork backtracking solutions.

--- schedwork.cpp
+++ schedwork.cpp
@@ -14,16 +14,61 @@
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
-static const Worker_T INVALID_ID = (unsigned int)-1;
-
-
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCounts,
+    const size_t idx
+);
 
 
 // Add your implementation of schedule() and other helper functions here
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCounts,
+    const size_t idx
+)
+{
+    const size_t n = avail.size();
+    if (idx == n * dailyNeed) {
+        return true;
+    }
+
+    const size_t day = idx / dailyNeed;
+    const size_t posInDay = idx % dailyNeed;
+    const size_t k = avail[day].size();
+
+    for (size_t w = 0; w < k; w++) {
+        if (!avail[day][w]) {
+            continue;
+        }
+        if (shiftCounts[w] >= maxShifts) {
+            continue;
+        }
+        if (find(sched[day].begin(), sched[day].begin() + posInDay, w)
+            != sched[day].begin() + posInDay) {
+            continue;
+        }
+
+        sched[day][posInDay] = w;
+        shiftCounts[w]++;
+
+        if (scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCounts, idx + 1)) {
+            return true;
+        }
+
+        shiftCounts[w]--;
+    }
+
+    return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -38,8 +83,15 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    const size_t n = avail.size();
+    const size_t k = avail[0].size();
 
+    sched.resize(n);
+    for (size_t day = 0; day < n; day++) {
+        sched[day].resize(dailyNeed);
+    }
 
+    vector<size_t> shiftCounts(k, 0);
 
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCounts, 0);
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -12,7 +12,14 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
+void wordleHelper(
+    size_t pos,
+    string& current,
+    const string& remainingFloating,
+    const string& in,
+    size_t blanksRemaining,
+    const set<string>& dict,
+    set<string>& results);
 
 
 // Definition of primary wordle function
@@ -21,8 +28,74 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    set<string> results;
+    if (in.empty()) {
+        return results;
+    }
 
+    size_t blanksRemaining = 0;
+    for (size_t i = 0; i < in.length(); i++) {
+        if (in[i] == '-') {
+            blanksRemaining++;
+        }
+    }
+
+    string current(in.length(), 'a');
+    wordleHelper(0, current, floating, in, blanksRemaining, dict, results);
+    return results;
 }
 
-// Define any helper functions here
+void wordleHelper(
+    size_t pos,
+    string& current,
+    const string& remainingFloating,
+    const string& in,
+    size_t blanksRemaining,
+    const set<string>& dict,
+    set<string>& results)
+{
+    if (remainingFloating.length() > blanksRemaining) {
+        return;
+    }
+
+    if (pos == in.length()) {
+        if (remainingFloating.empty() && dict.find(current) != dict.end()) {
+            results.insert(current);
+        }
+        return;
+    }
+
+    if (in[pos] != '-') {
+        current[pos] = in[pos];
+        string nextFloating = remainingFloating;
+        size_t idx = nextFloating.find(in[pos]);
+        if (idx != string::npos) {
+            nextFloating.erase(idx, 1);
+        }
+        wordleHelper(pos + 1, current, nextFloating, in, blanksRemaining, dict, results);
+        return;
+    }
+
+    if (blanksRemaining == remainingFloating.length()) {
+        for (size_t i = 0; i < remainingFloating.length(); i++) {
+            char c = remainingFloating[i];
+            current[pos] = c;
+            string nextFloating = remainingFloating;
+            nextFloating.erase(i, 1);
+            wordleHelper(pos + 1, current, nextFloating, in, blanksRemaining - 1, dict, results);
+        }
+        return;
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        current[pos] = c;
+        string nextFloating = remainingFloating;
+        size_t idx = nextFloating.find(c);
+        if (idx != string::npos) {
+            nextFloating.erase(idx, 1);
+        }
+        if (nextFloating.length() <= blanksRemaining - 1) {
+            wordleHelper(pos + 1, current, nextFloating, in, blanksRemaining - 1, dict, results);
+        }
+    }
+}
